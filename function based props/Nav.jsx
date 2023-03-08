8a0394ff0e355e5f717b08f76bc5b8795be673c6
Bhatsharvari <123368186+Bhatsharvari@users.noreply.github.com>
2023-03-08 23:44:16 +0530

    Add files via upload
    Added

---
+++ function based props/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Nav=(props)=>{
+    return(
+        <>
+       
+        {/* <h1>{props.data}</h1>
+        <h2>{props.password}</h2>  */}
+        </>
+        
+        
+        
+        
+
+    )
+}
+export default Nav
